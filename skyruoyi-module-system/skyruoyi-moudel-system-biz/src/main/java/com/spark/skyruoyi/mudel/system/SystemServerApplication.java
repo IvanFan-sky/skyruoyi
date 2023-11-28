22368214963390c1535f845f096cda23a4308e86
spark <fanliansy@qq.com>
2023-11-28 22:57:01 +0800

    skyruoyi 基础项目创建，统一依赖管理

---
+++ skyruoyi-module-system/skyruoyi-moudel-system-biz/src/main/java/com/spark/skyruoyi/mudel/system/SystemServerApplication.java
@@ -0,0 +1,18 @@
+package com.spark.skyruoyi.mudel.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author FanLian
+ * @Create 2023-11-28 22:42
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+public class SystemServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemServerApplication.class,args);
+    }
+}
+ 
